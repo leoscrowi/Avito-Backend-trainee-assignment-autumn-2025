d1a804d23113907e22f50772348138083dc8236d
Leonid <leonidkimschool@gmail.com>
2025-11-13 00:31:15 +0300

    fixed domain and added http errors

--- domain/pull_requests.go
+++ domain/pull_requests.go
@@ -1,9 +1,9 @@
 package domain
 
 import (
-	"github.com/google/uuid"
-
 	"time"
+
+	"github.com/google/uuid"
 )
 
 type Status int
@@ -13,20 +13,21 @@ const (
 	MERGED
 )
 
+// TODO: кажется, можно добавить nullable прям в
 type PullRequest struct {
-	PullRequestId     uuid.UUID
-	PullRequestName   string
-	AuthorId          uuid.UUID
-	Status            Status
-	AssignedReviewers []uuid.UUID
-	NeedMoreReviewers bool
-	CreatedAt         time.Time
-	UpdatedAt         time.Time
+	PullRequestId     uuid.UUID   `json:"pull_request_id"`
+	PullRequestName   string      `json:"pull_request_name"`
+	AuthorId          uuid.UUID   `json:"author_id"`
+	Status            Status      `json:"status"`
+	AssignedReviewers []uuid.UUID `json:"assigned_reviewers"`
+	NeedMoreReviewers bool        `json:"need_more_reviewers"`
+	CreatedAt         time.Time   `json:"created_at"`
+	UpdatedAt         time.Time   `json:"updated_at"`
 }
 
 type PullRequestShort struct {
-	PullRequestId   uuid.UUID
-	PullRequestName string
-	AuthorId        uuid.UUID
-	Status          Status
+	PullRequestId   uuid.UUID `json:"pull_request_id"`
+	PullRequestName string    `json:"pull_request_name"`
+	AuthorId        uuid.UUID `json:"author_id"`
+	Status          Status    `json:"status"`
 }
--- domain/users.go
+++ domain/users.go
@@ -3,7 +3,7 @@ package domain
 import "github.com/google/uuid"
 
 type User struct {
-	UserID   uuid.UUID `yml:"user_id"`
-	Username string    `yml:"username"`
-	IsActive bool      `yml:"is_active"`
+	UserID   uuid.UUID `json:"user_id"`
+	Username string    `json:"username"`
+	IsActive bool      `json:"is_active"`
 }
