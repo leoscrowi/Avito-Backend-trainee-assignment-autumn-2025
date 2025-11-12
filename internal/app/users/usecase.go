00ca990496a6f3f0bd4a7606ceb4c3e125fde0fb
Leonid <leonidkimschool@gmail.com>
2025-11-13 00:24:23 +0300

    added internal users structure

--- internal/app/users/usecase.go
+++ internal/app/users/usecase.go
@@ -1,1 +1,13 @@
 package users
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/leoscrowi/pr-assignment-service/domain"
+)
+
+type UseCase interface {
+	SetIsActive(ctx context.Context, userID uuid.UUID, isActive bool) error
+	GetReview(userID uuid.UUID) []domain.PullRequest
+}
