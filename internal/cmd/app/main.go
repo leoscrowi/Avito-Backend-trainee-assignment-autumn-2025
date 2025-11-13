8615ebcd988755d86338e2808dcab91f89f1b468
Leonid <leonidkimschool@gmail.com>
2025-11-13 09:16:19 +0300

    added migrations and repository

--- internal/cmd/app/main.go
+++ internal/cmd/app/main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"errors"
+	"log"
+	"os"
+
+	"github.com/golang-migrate/migrate/v4"
+)
+
+func main() {
+
+	// TODO: доделать dbUrl, сделать конфиг
+	dbUrl := "temp var"
+
+	migrations, err := migrate.New(
+		"file://migrations",
+		dbUrl,
+	)
+
+	if err != nil {
+		log.Println("Failed to create migration instance", err)
+		os.Exit(1)
+	}
+
+	if err = migrations.Up(); !errors.Is(err, migrate.ErrNoChange) {
+		log.Println("Failed to apply migrations", err)
+		os.Exit(1)
+	}
+}
